687cedc8d8538344f1de780cce7ec838f8325cef
hyunnn0105 <asha010519@gmail.com>
2022-05-22 23:31:30 +0900

    220522

--- src/coding01/chap03Loop/Bj11021ab7.java
+++ src/coding01/chap03Loop/Bj11021ab7.java
@@ -11,8 +11,23 @@ public class Bj11021ab7 {
         for (int i = 0; i < t; i++){
             int A = sc.nextInt();
             int B = sc.nextInt();
-            System.out.printf("case #%d : %d\n", i+1, A+B);
-        }
+            System.out.printf("Case #"+(i+1)+": "+(A+B));
 
+        }
+        sc.close();
     }
 }
+/*
+    import java.util.*;
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            for (int i = 0; i < t; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                System.out.println("Case #" + (i + 1) + ": " + (a + b));
+            } sc.close();
+        }
+    }
+ */
---
+++ src/coding01/chap03Loop/Bj11022ab8.java
@@ -0,0 +1,19 @@
+package coding01.chap03Loop;
+
+import java.util.Scanner;
+
+public class Bj11022ab8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                int t = sc.nextInt();
+                for (int i = 0; i < t; i++) {
+                    int a = sc.nextInt();
+                    int b = sc.nextInt();
+                    System.out.println("Case #" + (i + 1) + ": " + (a) + "+" + (b) "=" + (a + b));
+
+
+                } sc.close();
+            }
+        }
+    }
+}
