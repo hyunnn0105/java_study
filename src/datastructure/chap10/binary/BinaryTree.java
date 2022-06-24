b781502cea5c3ac9edeb8a8e9979bbc6a8b4162d
hyunnn0105 <asha010519@gmail.com>
2022-06-24 18:18:59 +0900

    220623 tree구조

--- src/datastructure/chap10/binary/BinaryTree.java
+++ src/datastructure/chap10/binary/BinaryTree.java
@@ -42,6 +42,8 @@ public class BinaryTree {
 
     private Node root; // 트리의 루트 노드 - l와 r가 루트저장
 
+
+
     // ================ 삽입 =====================
     public void add(int data){
         // 삽입 데이터 노드 생성
@@ -59,6 +61,21 @@ public class BinaryTree {
                 //백업
                 parent = current;
                 // 삽입할 데이터가 타겟노드의 데이터보다 작은경우
+                if (data < current.getData()) {
+                    current = current.getLeftChild();
+                    if (current == null) {
+                        parent.setLeftChild(newNode);
+                        return;
+                    }
+                } else { // 큰 경우
+                    current = current.getRightChild();
+                    if (current == null) {
+                        parent.setRightChild(newNode);
+                        return;
+                    }
+                }
+
+                /*
                 if (data < current.getData()){
                     current = current.getLeftChild();
                 } else {
@@ -69,8 +86,11 @@ public class BinaryTree {
                         return;
                     }
                 }
+
+                 */
             }
         }
+
     }
     
     
@@ -88,4 +108,183 @@ public class BinaryTree {
         }
     }
 
+    // 중위순회 (in order) - 전중후
+    public void inOrder(Node tempRoot){
+        if (tempRoot != null) {
+            preOrder(tempRoot.getLeftChild());
+            // 1번출력
+            System.out.printf("%d ", tempRoot.getData());
+            // 2가 temp 루트가 됨 - 재귀호출 반복
+            preOrder(tempRoot.getRightChild());
+        }
+    }
+
+
+    // 후위 순회 (post order) - 전후중
+    public void postOrder(Node tempRoot) {
+        if (tempRoot != null) {
+            postOrder(tempRoot.getLeftChild());
+            postOrder(tempRoot.getRightChild());
+            System.out.printf("%d ", tempRoot.getData());
+        }
+    }
+
+
+    public Node find(int targetData){
+        Node current = root;
+        while (true){
+            if (current == null) return null; // 탐색실패
+            // 찾는 값이 현재의 노드보다 작은경우
+            if (targetData < current.getData()){
+                current = current.getLeftChild();
+            } else {
+                current = current.getRightChild();
+            }
+
+            // 탐색성공
+            return current;
+        }
+
+
+    }
+
+    // 최대값 찾기
+    public Node findMin(){
+        // 루트 확인
+                if (isEmpty()) return null;
+
+        Node current = root;
+        while (current.getLeftChild() != null){
+            current = current.getLeftChild();
+        }
+        return current;
+
+    }
+
+    // 최소값 찾기
+    public Node findMax(){
+                if (isEmpty()) return null;
+
+        Node current = root;
+        while (current.getRightChild() != null){
+            current = current.getRightChild();
+        }
+        return current;
+    }
+
+    // ============================= 삭제 ===============================
+
+    public boolean delete(int targetData){
+        // 삭제노드와 해당 삭제노드의 부모노드를 탐색
+        Node current = root;
+        Node parent = current;
+        
+        while (targetData != current.getData()){
+            if (current == null) return false;
+            
+            parent = current;
+            if (targetData < current.getData()){
+                current = current.getLeftChild();
+            } else {
+                current = current.getRightChild();
+            }
+        }
+        
+        // 삭제 진행 - 3가지 분기점
+        // 삭제 대상노드의 자식이 없는 경우
+        if (current.getLeftChild() == null
+                && current.getRightChild() == null){
+            // 루트가 삭제대상
+            if (current == root) {
+                root = null;
+            } else if (parent.getRightChild() == current) {
+                // 삭제 대상이 부모의 오른쪽 자식이였다면
+                parent.setRightChild(null);
+            } else{
+                parent.setLeftChild(null);
+            }
+
+        }
+        // 삭제대상노드의 자식이 하나인 경우 - 왼쪽자식인 경우
+        else if (current.getRightChild() == null){
+            // 삭제 대상이 루트
+            if (current == root) {
+                root = current.getLeftChild();
+                // 삭제대상이 부모의 왼쪽 자식일 경우
+            } else if (current == parent.getLeftChild()) {
+                // 부모의 새로운 왼쪽 자식으로 삭제대상의 자식을 연결
+                parent.setLeftChild(current.getLeftChild());
+                // 삭제대상이 부모의 오른쪽 자식인경우
+            } else {
+                // 부모의 새로운 오른쪽 자식으로 삭제대상의 자식을 연결
+                parent.setRightChild(current.getRightChild());
+
+            }
+        }
+        // 삭제대상노드의 자식이 하나인 경우 - 오른쪽 자식인 경우 ===== 놓쳤다
+        else if (current.getLeftChild() == null) {
+            // 삭제 대상이 루트
+            if (current == root) {
+                root = current.getLeftChild();
+                // 삭제대상이 부모의 왼쪽 자식일 경우
+            } else if (current == parent.getLeftChild()) {
+                // 부모의 새로운 왼쪽 자식으로 삭제대상의 자식을 연결
+                parent.setLeftChild(current.getLeftChild());
+                // 삭제대상이 부모의 오른쪽 자식인경우
+            } else {
+                // 부모의 새로운 오른쪽 자식으로 삭제대상의 자식을 연결
+                parent.setRightChild(current.getRightChild());
+
+            }
+        }
+        // ***삭제대상 노드의 자식이 둘인경우
+        // 후보노드 찾기! : 삭제될 노드보다 큰 키값을 가진 노드 중 가장 작은 키 값을 갖는 노드
+        else {
+            // 삭제노드보다 큰 애들중ㅇㅔ서 가장 작은애들 찾기
+            Node candidate = getCandidate(current);
+
+            if (current == root){
+                root = candidate;
+
+            } else if (current == parent.getLeftChild()){
+                parent.setLeftChild(candidate);
+            } else {
+                parent.setRightChild(candidate);
+            }
+            // 삭제노드의 왼쪽 자식을 후보노드의 왼쪽 자식으로 연결 
+            candidate.setLeftChild(current.getLeftChild());
+        }
+
+
+    }
+
+    // 후보노드 찾기
+    // : 삭제될 노드보다 큰 키값을 가진 노드 중 가장 작은 키 값을 갖는 노드
+    private Node getCandidate(Node deleteNode){
+        // 삭제노드보다 큰 애들중ㅇㅔ서 가장 작은애들 찾기
+        Node candidate = deleteNode.getRightChild();
+
+        // 후보노드는 삭제노드의 오른쪽 자식중에 가장 왼쪽 끝에있는 자식
+        while (candidate.getLeftChild() != null){
+            candidate = candidate.getLeftChild();
+        }
+        return candidate;
+    }
+
+    // 빈트리인지 확인 ** 수정하자
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+
+    public Node getRoot() {
+        return root;
+    }
+
+
+    
+    
+    
+    
+    
 }
--- src/datastructure/chap10/binary/Test.java
+++ src/datastructure/chap10/binary/Test.java
@@ -1,9 +1,44 @@
 package datastructure.chap10.binary;
 
+import Utility.Util;
+
 public class Test {
 
     public static void main(String[] args) {
 
         BinaryTree tree = new BinaryTree();
+        /*
+                              50
+                27                              68
+        12              36              55              82
+    7      19      **       49      **      **      76      **
+
+         */
+        tree.add(50);
+        tree.add(27);
+        tree.add(68);
+        tree.add(12);
+        tree.add(36);
+        tree.add(55);
+        tree.add(82);
+        tree.add(7);
+        tree.add(19);
+        tree.add(49);
+        tree.add(76);
+
+        tree.preOrder(tree.getRoot()); // 50 27 12 7 19 36 49 68 55 82 76
+        Util.line();
+        tree.inOrder(tree.getRoot());
+        Util.line();
+        tree.postOrder(tree.getRoot());
+
+        System.out.println("========= 탐색 =========");
+        Node found = tree.find(27);
+        System.out.println(found.toString());
+        System.out.println("========= 최대최소 =========");
+        System.out.println("최대"+tree.findMax());
+        System.out.println("최소"+tree.findMin());
+
+
     }
 }
