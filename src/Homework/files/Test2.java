83e3610d934143e4198c5197d2c9f5149e7ab79c
hyunnn0105 <asha010519@gmail.com>
2022-06-30 23:33:10 +0900

    220630 home

--- src/Homework/files/Test2.java
+++ src/Homework/files/Test2.java
@@ -3,34 +3,36 @@ package Homework.files;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test2 {
     public static void main(String[] args) throws IOException {
 
+// 파일 불러오기
         BufferedReader reader = new BufferedReader(
                 new FileReader("C:/intellij/IdeaProjects/java_study/src/Homework/files/stream-data.txt")
         );
         String str;
-        while ((str = reader.readLine()) != null){
-            System.out.println(str);
 
+        // list
+        List<String> strArr = new ArrayList<>();
+
+        while ((str = reader.readLine()) != null){
+            strArr.add(str);
         }
         reader.close();
 
-        /*
-        String file = "C:/intellij/IdeaProjects/java_study/src/Homework/files";
+        // stream
+        Stream<String> stream = strArr.stream();
 
-        try {
-            FileInputStream inputStream = new FileInputStream(file);
+        List<Integer> integerList = stream.mapToInt(Integer::parseInt)
+                .collect(Collectors.toList());
 
-            while (inputStream.available()>0){
-                System.out.println((int) inputStream.read());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-         */
-
+        System.out.println(integerList);
     }
+
+
 }
