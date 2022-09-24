64b86504a9a4192aad943d8605e73f1cfe27b78e
hyunnn0105 <asha010519@gmail.com>
2022-09-24 23:38:57 +0900

    220704 - readMe 수정

--- src/JavaStudyDate/day05/roulette.java
+++ src/JavaStudyDate/day05/roulette.java
@@ -1,19 +1,40 @@
 package JavaStudyDate.day05;
 
+import java.util.Scanner;
+
 public class roulette {
     public static void main(String[] args) {
-        // 게임인원 등록 - 입력
-        int gameMember = 4;
+        Scanner sc = new Scanner(System.in);
+        int gameMember = 0;
+        while (true) {
+            // 게임인원 등록 - 입력
+            System.out.println("게임 인원을 2~4 사이로 등록하세요");
+            gameMember = sc.nextInt();
+            if ( gameMember >= 2 && gameMember <= 4) {
+                System.out.println(gameMember + "인원수를 입력했습니다.");
+            } else {
+                System.out.println("인원수를 다시 입력해주세요");
+                return;
+            }
 
-        // 플레이어 이름등록
-        String[] playerName = {"playA", "playB", "playC", "playD"};
 
+            // 플레이어 이름등록
+            String[] playerName = new String[gameMember];
+            for (int i = 1; i < playerName.length + 1; i++) {
+                System.out.println(i + "번째 player 이름을 입력하세요");
+                String name = sc.next();
+                playerName[i] = name;
+            }
+//        String[] playerName = {"playA", "playB", "playC", "playD"};
+
+
+
+    /*
         // 실탄의 개수 추가
         int bullet = 1;
 
         // 배열은 6개탄창 ======== 배열을 랜덤으로 바꾸기??
         int[] bbang = {1, 1, 1, 1, 1, -1};
-        //
 
         // 총 돌리기(무작위 실행)
         int random = (int) ((Math.random()*5)-1);
@@ -45,5 +66,7 @@ public class roulette {
 
     }
 
-    
+     */
+
+    }
 }
